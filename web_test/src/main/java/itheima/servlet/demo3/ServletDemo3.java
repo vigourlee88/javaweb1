a54ff27788a32673fe979b95d3001879f2f59b09
vigourlee88 <vigourlee137@gmail.com>
2022-11-22 18:07:16 +0900

    demo

---
+++ web_test/src/main/java/itheima/servlet/demo3/ServletDemo3.java
@@ -0,0 +1,30 @@
+package itheima.servlet.demo3;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *使用eclipse 进行开发 Servlet 
+ *不用在web.xml中修改，自动完成Servlet的配置
+ */
+/**
+ * @author jack
+ *
+ */
+public class ServletDemo3 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
