1c5346c9d25f981bb1b2825e9ed7328c3e71daab
vigourlee88 <vigourlee137@gmail.com>
2022-12-29 17:15:19 +0900

    demo

---
+++ ajax/src/main/java/com/itheima/servlet/ProvinceServlet.java
@@ -0,0 +1,29 @@
+package com.itheima.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itheima.util.CityUtil;
+
+
+public class ProvinceServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //得到省份信息
+		String province = CityUtil.getProvince();
+		
+		//写回到浏览器，涉及到中文乱码
+		response.setCharacterEncoding("utf-8");
+		response.getWriter().write(province);
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
