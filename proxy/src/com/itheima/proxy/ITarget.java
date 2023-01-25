07cc5c16374dae56afb0d79a879bbcbe2b1ff583
vigourlee88 <vigourlee137@gmail.com>
2023-01-25 18:36:06 +0900

    demo

---
+++ proxy/src/com/itheima/proxy/ITarget.java
@@ -0,0 +1,8 @@
+package com.itheima.proxy;
+//抽象角色
+public interface ITarget {
+	
+	
+	public void show();
+		
+}
---
+++ proxy/src/com/itheima/service/IUserService.java
@@ -0,0 +1,8 @@
+package com.itheima.service;
+
+public interface  IUserService {
+	
+	public String addUser(String username,String password);
+	
+
+}
